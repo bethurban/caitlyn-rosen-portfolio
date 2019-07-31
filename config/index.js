71045cbd1e6cad932fb3d2e858bb414ded47b2e1
bethurban <beth.urban@gmail.com>
2019-07-31 15:36:15 -0400

    Update favicon and other icons.

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   siteLanguage: 'en', // Language Tag on <html> element
   siteHeadline: 'Caitlyn Rosen portfolio', // Headline for schema.org JSONLD
   // siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
-  favicon: 'src/favicon.ico', // Your image for favicons. You can find it in the /src folder
+  // favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: 'I am a journalism student with an emphasis in investigative reporting at the University of Missouri School of Journalism.', // Your site description
   author: 'Beth Urban', // Author for schemaORGJSONLD
   // siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
