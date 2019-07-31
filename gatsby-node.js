012e69e1f30eb052d9d1936b19920bac92f80b88
bethurban <beth.urban@gmail.com>
2019-07-31 15:05:26 -0400

    Update gatsby-node to account for unnecessary components/files.

--- gatsby-node.js
+++ gatsby-node.js
@@ -30,66 +30,66 @@ exports.onCreateNode = ({ node, actions }) => {
   }
 }
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//
+//   // const postTemplate = require.resolve('./src/templates/post.js')
+//   // const categoryTemplate = require.resolve('./src/templates/category.js')
+//
+//   const result = await wrapper(
+//     graphql(`
+//       {
+//         allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+//           nodes {
+//             fields {
+//               slug
+//             }
+//             frontmatter {
+//               title
+//               categories
+//             }
+//           }
+//         }
+//       }
+//     `)
+//   )
 
-  const postTemplate = require.resolve('./src/templates/post.js')
-  const categoryTemplate = require.resolve('./src/templates/category.js')
-
-  const result = await wrapper(
-    graphql(`
-      {
-        allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
-          nodes {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-              categories
-            }
-          }
-        }
-      }
-    `)
-  )
-
-  const posts = result.data.allMdx.nodes
-
-  posts.forEach((n, index) => {
-    const next = index === 0 ? null : posts[index - 1]
-    const prev = index === posts.length - 1 ? null : posts[index + 1]
-
-    createPage({
-      path: n.fields.slug,
-      component: postTemplate,
-      context: {
-        slug: n.fields.slug,
-        prev,
-        next,
-      },
-    })
-  })
-
-  const categorySet = new Set()
-
-  _.each(posts, n => {
-    if (_.get(n, 'frontmatter.categories')) {
-      n.frontmatter.categories.forEach(cat => {
-        categorySet.add(cat)
-      })
-    }
-  })
-
-  const categories = Array.from(categorySet)
-
-  categories.forEach(category => {
-    createPage({
-      path: `/categories/${_.kebabCase(category)}`,
-      component: categoryTemplate,
-      context: {
-        category,
-      },
-    })
-  })
-}
+//   const posts = result.data.allMdx.nodes
+//
+//   posts.forEach((n, index) => {
+//     const next = index === 0 ? null : posts[index - 1]
+//     const prev = index === posts.length - 1 ? null : posts[index + 1]
+//
+//     createPage({
+//       path: n.fields.slug,
+//       component: postTemplate,
+//       context: {
+//         slug: n.fields.slug,
+//         prev,
+//         next,
+//       },
+//     })
+//   })
+//
+//   const categorySet = new Set()
+//
+//   _.each(posts, n => {
+//     if (_.get(n, 'frontmatter.categories')) {
+//       n.frontmatter.categories.forEach(cat => {
+//         categorySet.add(cat)
+//       })
+//     }
+//   })
+//
+//   const categories = Array.from(categorySet)
+//
+//   categories.forEach(category => {
+//     createPage({
+//       path: `/categories/${_.kebabCase(category)}`,
+//       component: categoryTemplate,
+//       context: {
+//         category,
+//       },
+//     })
+//   })
+// }
